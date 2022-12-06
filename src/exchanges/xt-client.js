44ab216e48b6ac1353d1ebf6143b688a9aaca245
Gašper Košmrlj <gasperk@abelium.eu>
2022-12-06 20:35:31 +0100

    upgrade xt

--- src/exchanges/xt-client.js
+++ src/exchanges/xt-client.js
@@ -2,59 +2,56 @@ const BasicClient = require("../basic-client");
 const Trade = require("../trade");
 const Ticker = require("../ticker");
 const zlib = require("zlib");
-const moment = require('moment-timezone');
+//const moment = require('moment-timezone');
 
 class XtClient extends BasicClient {
   /**
     Documentation:
     https://github.com/xtpub/api-doc/blob/master/websocket-api-v1-en.md
    */
-  constructor({ wssPath = "wss://xtsocket.xt.com/websocket", watcherMs } = {}) {
+  constructor({ wssPath = "wss://stream.xt.com/public", watcherMs } = {}) {
     super(wssPath, "Xt", undefined, watcherMs);
     this.hasTickers = true;
     this.hasTrades = true;
     this.hasCandles = false;
     this.hasLevel2Updates = false;
     this.constructL2Price = false;
-    setInterval(this._sendPing.bind(this), 15*1000);
+    this.id = 1;
+    this.debounceWait = 500;
+    this._debounceHandles = new Map();
+    setInterval(this._sendPing.bind(this), 30*1000);
   }
 
   _sendPing() {
     if (this._wss) {    
-      this._wss.send(
-        JSON.stringify({
-          ping: Date.now()
-        })
-      );
+      this._wss.send("ping");
     }
   }
 
-  _sendPong(unix) {
-    if (this._wss) {    
-      this._wss.send(
-        JSON.stringify({
-          pong: unix
-        })
-      );
-    }
+  _debounce(type, fn) {
+    clearTimeout(this._debounceHandles.get(type));
+    this._debounceHandles.set(type, setTimeout(fn, this.debounceWait));
   }
 
   _sendSubTrades(remote_id) {
-    this._wss.send(
-      JSON.stringify({
-        channel: "ex_last_trade",
-        market: remote_id,
-        event: "addChannel"
-      })
-    );
+    this._debounce("trade.subscribe", () => {
+      let params = Array.from(this._tradeSubs.keys()).map(x => `trade@${x}`);
+      this._wss.send(
+        JSON.stringify({
+            method: "subscribe", 
+            params, 
+            id: this.id++
+        })
+      );
+    });
   }
 
   _sendUnsubTrades(remote_id) {
     this._wss.send(
       JSON.stringify({
-        channel: "ex_last_trade",
-        market: remote_id,
-        event: "removeChannel"
+          method: "unsubscribe", 
+          params: [`trade@${remote_id}`], 
+          id: this.id++
       })
     );
   }
@@ -68,56 +65,35 @@ class XtClient extends BasicClient {
   }
 
   _onMessage(msg) {
-    // console.log(msg);
+    console.log(msg);
+    if(msg == 'pong') {
+      this.emit('ping');
+      return;
+    }
     let message = JSON.parse(msg);
 
-    if(message.ping) {
-      this.emit('ping');
-      this._sendPong(message.ping);
-    } else if(message.pong) {
-      this.emit('ping');      
-    } else if(message.data && message.data.channel == 'ex_last_trade') {
-      for(let datum of message.data.records) {
-        let market = this._tradeSubs.get(message.data.market);
-        if(market) {
-          let trade = this._constructTrades(datum, market);
-          this.emit("trade", trade, market);
-        }
+    if(message.topic == 'trade' && message.data) {
+      let market = this._tradeSubs.get(message.data.s);
+      if(market) {
+        let trade = this._constructTrades(message.data, market);
+        this.emit("trade", trade, market);
       }
-    } else if(message.method == 'market_update') {
-
     }
   }
 
 
   _constructTrades(datum, market) {
-    let [ unix, price, amount, side, tradeId ] = datum;
+    let { i, t, p, q, b } = datum;
     return new Trade({
       exchange: "Xt",
       base: market.base,
       quote: market.quote,
       id: market.id,
-      tradeId,
-      unix,
-      side: side == 'bid' ? 'buy' : 'sell',
-      price,
-      amount
-    });
-  }
-
-  _constructTicker(datum, market) {
-    let { period, last, open, close, high, low, volume, deal } = datum;
-    return new Ticker({
-      exchange: "Xt",
-      base: market.base,
-      quote: market.quote,
-      timestamp: Date.now(),
-      last,
-      open,
-      close,
-      high,
-      low,
-      volume
+      tradeId: i,
+      unix: t,
+      side: b ? 'buy' : 'sell',
+      price: p,
+      amount: q
     });
   }
 
